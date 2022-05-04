63deeeddc16f1442237acd587cc77027a0dc9360
Devilish134 <kamilmusialik22@gmail.com>
2022-05-04 13:56:29 +0200

    add tests and fix PLN to USD function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,10 @@ import styles from './Header.module.scss';
 
 const Header = () => (
   <header>
-    <h1 className={styles.title}>CurrencyApp</h1>
+    <h1 className={styles.title}>
+      CurrencyApp
+    </h1>
   </header>
 );
 
 export default Header;
-
